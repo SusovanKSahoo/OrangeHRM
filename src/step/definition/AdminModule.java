f0fd9d33dd0a529af71e8f463f3baeb592cf2ccf
Susovan <susovan.793@gmail.com>
2018-01-05 22:23:46 +0530

    AdminModule script logger class removed

--- src/step/definition/AdminModule.java
+++ src/step/definition/AdminModule.java
@@ -2,7 +2,6 @@ package step.definition;
 
 import java.io.IOException;
 
-import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,21 +9,18 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.log4testng.Logger;
 
 import cucumber.api.java.en.When;
 import utility.PropertyFileUtility;
 
 public class AdminModule {
 	WebDriver driver = AbstractPage.getDriver();
-	static Logger log = Logger.getLogger(AdminModule.class);
+	
 	
 	@When("^wait for an element \"([^\"]*)\"$")
 	public void wait_for_element(String arg1) throws IOException {
 		WebDriverWait wait = new WebDriverWait(driver, 25);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(PropertyFileUtility.getElementValue(arg1))));
-		PropertyConfigurator.configure("log4j.xml");
-		log.info("Waiting for element "+arg1+".");
 	}
 	
 	@When("^mouse over to the element \"([^\"]*)\"$")
@@ -32,19 +28,16 @@ public class AdminModule {
 		Actions action = new Actions(driver);
 		WebElement element = driver.findElement(By.xpath(PropertyFileUtility.getElementValue(arg1)));
 		action.moveToElement(element).build().perform();
-		log.info("Mouse moved to "+arg1);
 	}
 	
 	@When("^click on the element \"([^\"]*)\"$")
 	public void click_on_element(String arg1) throws IOException {
 		driver.findElement(By.xpath(PropertyFileUtility.getElementValue(arg1))).click();
-		log.info(arg1+" is clicked.");
 	}
 	
 	@When("^enter a value \"([^\"]*)\" \"([^\"]*)\"$")
 	public void enter_value(String arg1, String arg2) throws IOException {
 		driver.findElement(By.xpath(PropertyFileUtility.getElementValue(arg1))).sendKeys(arg2);
-		log.info(arg2+" value is entered in "+arg1+" field.");
 	}
 	
 	@When("^select an element from dropdown \"([^\"]*)\" \"([^\"]*)\"$")
@@ -52,6 +45,5 @@ public class AdminModule {
 		WebElement dropBox = driver.findElement(By.xpath(PropertyFileUtility.getElementValue(arg1)));
 		Select dropDown = new Select(dropBox);
 		dropDown.selectByVisibleText(arg2);
-		log.info(arg2+" is selected from the drop-down.");
 	}
 }
